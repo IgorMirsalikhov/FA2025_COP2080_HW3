dad5a22ad24466362a3d86c409da4c87b2b758e4
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2025-09-20 16:22:24 -0400

    Create funcs.py

---
+++ funcs.py
@@ -0,0 +1,24 @@
+### Problem 1
+def remove_min_max(data: set[int], total: int) -> set[int]:
+    pass
+
+### Problem 2
+def seven_segment(lit_seq: set[int]) -> list[int]:
+    """Finding two-digit numbers that seven digit display may be displaying"""
+    pass
+
+### Problem 3
+def generate_html_tag(tag_name: str, content: str, **kwargs):
+    pass
+
+### Problem 4
+def check_negative(z):
+    pass
+
+### Problem 5. Make a lambda function instead 
+def even_or_odd(x):
+    pass
+
+### Problem 6
+def conv_aggr(data: list[tuple[str, int]]) -> dict[str, int]:
+    pass
